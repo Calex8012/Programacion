d18d18c9b76f09c87a2bd33bb911d020294ac241
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2018-10-23 17:07:37 -0500

    Create serpientes.cpp

---
+++ serpientes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+int lanzardado();
+
+int main()
+{
+    int vector[101];
+    int j1=0,j2=0;
+    int llenar vector(vector, 101);
+    while(j1<100 && j2<100)
+    {
+        j1+=lanzardado();
+        if
+        j1+=vector[j1];
+        j2+=lanzardado();
+        if
+        j2+=vector[j2];
+    }
+
+    return 0;
+}
+int lanzardado()
+{
+    int dado;
+    srand(time(0)); 
+    dado=rand()%6+1;
+   
+    return dado;
+}
