8721405fcc7c316db4f0763a5d2978e65217d16a
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2018-10-30 16:21:38 -0600

    Update vector-1.cpp

--- vector-1.cpp
+++ vector-1.cpp
@@ -6,14 +6,14 @@ int main()
 {
     int tam=25,i,pos;
     int vector[25]={1,2,2,2,2,3,1,1,1,2,4,4,4,2,5,5,2,2,3,3,3,4,4,4,5};
-    for(i=0;i<25;i++)
+    for(i=0;i<25 && vector[i]!=-1;i++)
     {
-        for(pos=i+1;pos<24;pos++)
+        pos=i+1;
+        while(pos<25 && vector[i]==vector[pos])
         {
-            if(i==pos)
-            {
-                recorrer(vector, tam, pos);
-            }
+            //12312415261-1 -1 -1 -1 -1 -1-1-1
+            recorrer(vector, tam, pos);
+            
         }
     }
     for(int i=0;i<24;i++)
@@ -25,7 +25,8 @@ int main()
 void recorrer(int vector[], int tam, int pos){
     for(int i=pos;i<tam-1;i++){
         vector[i]=vector[i+1];
-        vector[tam-1]=-1;
+        
     }
+    vector[tam-1]=-1;
 
 }
